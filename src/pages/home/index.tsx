c6100b5a8752620eb3afa0662550926fbccb01f3
WJQ <1092229612@qq.com>
2023-09-15 13:09:24 +0800

    feat:修改蓝牙扫描逻辑

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,38 +1,79 @@
 import { View } from '@tarojs/components'
 import './index.less'
-import { Button } from '@nutui/nutui-react-taro'
+import { Button, VirtualList } from '@nutui/nutui-react-taro'
 import Taro from '@tarojs/taro'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const Home: React.FC = () => {
-  const [res, setRes] = useState<any>('')
+  const [flag, setFlag] = useState(true)
+  const btList = useRef<any[]>([])
+  const [pageNo, setPageNo] = useState(1)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const itemVariable = (data: any, dataIndex: number) => {
+    return (
+      <div
+        style={{
+          height: `${dataIndex % 2 === 0 ? '100px' : '50px'}`,
+        }}>
+        {data.deviceId}
+      </div>
+    )
+  }
+  const onScroll = () => {
+    if (pageNo > 50 || isLoading) return
+    setIsLoading(true)
+    setTimeout(() => {
+      setPageNo(pageNo + 1)
+      setIsLoading(false)
+    }, 30)
+  }
+
   const ab2hex = (buffer) => {
     var hexArr = Array.prototype.map.call(new Uint8Array(buffer), function (bit) {
       return ('00' + bit.toString(16)).slice(-2)
     })
     return hexArr.join('')
   }
-  const initBlue = () => {
-    Taro.openBluetoothAdapter({
-      success: function (res) {
-        Taro.startBluetoothDevicesDiscovery({
-          success: function (res) {
-            Taro.onBluetoothDeviceFound(function (res) {
-              var devices = res.devices
-              console.log('new device list has founded')
-              console.dir(devices)
-              console.log(ab2hex(devices[0].advertisData))
-            })
-          },
-        })
-      },
+  const initBlue = async () => {
+    await Taro.openBluetoothAdapter()
+    await Taro.startBluetoothDevicesDiscovery()
+
+    Taro.onBluetoothDeviceFound(async function (res) {
+      var devices = res.devices
+      console.log('new device list has founded')
+      console.dir(devices)
+      console.log(ab2hex(devices[0].advertisData))
+      const newRes = devices[0]
+      let shouldBeAdd = true
+      btList.current.forEach((element) => {
+        if (element.deviceId === newRes.deviceId || !newRes.connectable || !newRes.name) {
+          shouldBeAdd = false
+        }
+      })
+      if (shouldBeAdd) {
+        // setBtList(btList.push(newRes))
+        btList.current.push(newRes)
+      }
+      if (btList.current.length === 3) {
+        const success = await Taro.stopBluetoothDevicesDiscovery()
+      }
+      console.log(btList)
     })
   }
   return (
     <View className="home">
       <View className="Home">主页</View>
       <Button onClick={() => initBlue()}>初始化蓝牙</Button>
-      {res}
+      <VirtualList
+        itemHeight={80}
+        list={btList.current}
+        itemRender={itemVariable}
+        onScroll={onScroll}
+        itemEqual={false}
+        containerHeight={500}
+      />
+      {btList.current[0]?.deviceId}
     </View>
   )
 }
